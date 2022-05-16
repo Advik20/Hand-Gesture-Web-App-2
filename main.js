61ec8cfff081fd080bdd57bd98feab98b474e26b
Advik V <83173340+Advik20@users.noreply.github.com>
2022-05-16 19:33:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ function take_snapshot()
 
 console.log('ml5 version: ', ml5.version);
 
-classifier = imageClassifier('https://teachablemachine.withgoogle.com/models/wEYVOmUcx/model.json',modelLoaded);
+classifier = imageClassifier('https://teachablemachine.withgoogle.com/models/3Hb8L6VmI/model.json',modelLoaded);
 
 function modelLoaded() {
     console.log('Model Loaded!');
